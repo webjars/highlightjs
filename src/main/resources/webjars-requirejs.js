284e3a4dd59cad49ba6b1258cf1d96844750b634
James Ward <james@jamesward.org>
2014-03-29 09:51:43 -0600

    cleanup

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,11 +1,4 @@
-/*global requirejs */
-
-// Ensure any request for this webjar brings in jQuery.
 requirejs.config({
     paths: { "highlightjs": webjars.path("highlightjs", "hightlight.min") },
-    shim: { 
-        'highlightjs':  { 
-            'exports': 'hljs'
-        } 
-    }
+    shim: { "highlightjs": { "exports": "hljs" } }
 });
