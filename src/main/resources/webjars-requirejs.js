5f7e61574055097aed7f561b82c1819e9e38fb6a
James Ward <james@jamesward.org>
2014-03-30 08:52:12 -0600

    fix typo

--- src/main/resources/webjars-requirejs.js
+++ src/main/resources/webjars-requirejs.js
@@ -1,4 +1,4 @@
 requirejs.config({
-    paths: { "highlightjs": webjars.path("highlightjs", "hightlight.min") },
+    paths: { "highlightjs": webjars.path("highlightjs", "highlight.min") },
     shim: { "highlightjs": { "exports": "hljs" } }
 });
